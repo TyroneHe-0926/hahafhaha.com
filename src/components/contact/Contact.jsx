49d2223fbd62f0c9f1b6bd9805ba4a6c06c78991
TyroneHe-0926 <tyrone99926@gmail.com>
2024-10-26 02:51:37 -0400

    fix?: trying to resolve contact display issue on iphone chrome

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -14,8 +14,8 @@ function Contact() {
 
         <div className="card-container">
             <div className="card-inner-container">
-                <p className="card">Phone: +1(778)988-2087</p>
-                <p className="card">Email: tyrone99926@gmail.com</p>
+                <p className="card">Phone (778)988-2087</p>
+                <p className="card">Gmail tyrone99926</p>
                 <p className="card">LinkedIn: Qirong He</p>
             </div>
             <div className="card-inner-container">
