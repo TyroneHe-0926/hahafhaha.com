faa606823d3f8f9f307a8eb6e687a5dc29ef67b7
TyroneHe-0926 <tyrone99926@gmail.com>
2026-08-02 04:40:38 -0400

    refactor(gallery): extract photo data into photos.js

---
+++ src/components/gallery/photos.js
@@ -0,0 +1,44 @@
+// Single source of truth for the homepage photo gallery.
+// Adding a photo = appending one object here.
+export const photos = [
+  {
+    src: "/images/gallery/lake-louise.jpg",
+    title: "Lake Louise, Alberta",
+    date: "July 12, 2020",
+    iso: "2020-07-12",
+    story:
+      "I went on a road trip from Vancouver to Calgary during Covid in 2020. This was a picture I took in front of the famous Lake Louise.",
+  },
+  {
+    src: "/images/gallery/toronto-1.jpg",
+    title: "Toronto, Ontario",
+    date: "February 25, 2024",
+    iso: "2024-02-25",
+    story:
+      "Caught a crazy sunset in Toronto. I was interning for RBC back then and lived in downtown Toronto for around 4 months. Can't say I like Toronto that much, but at the time of this picture, I just escaped from Waterloo after being in school for 1 year, so Toronto felt like a breath of fresh air.",
+  },
+  {
+    src: "/images/gallery/stown-1.jpg",
+    title: "Shanghai, China",
+    date: "December 19, 2021",
+    iso: "2021-12-19",
+    story:
+      "This is the view from the Shanghai Citrix office at Lu Jia Zui. I was visiting a couple of other Shanghai tech company offices including Microsoft and eBay. All of the offices looks really nice, and lots of good food choices nearby.",
+  },
+  {
+    src: "/images/gallery/whiterock-1.jpg",
+    title: "White Rock, B.C.",
+    date: "December 22, 2023",
+    iso: "2023-12-22",
+    story:
+      "The Whiterock Pier at night. The pier is only a 10-min drive from my house, we sometimes just go and take a little walk near the shore. This was just a bit before Christmas as well so I think there was some sort of event going on. Lots of decorations that night.",
+  },
+  {
+    src: "/images/gallery/waterloo-1.jpg",
+    title: "Waterloo, Ontario",
+    date: "September 16, 2023",
+    iso: "2023-09-16",
+    story:
+      "There is a park near the Waterloo campus, and there's a small zoo within the park. There are usually some alpacas in there, I was rocking the same hair style as my dude here in the picture back then.",
+  },
+];
