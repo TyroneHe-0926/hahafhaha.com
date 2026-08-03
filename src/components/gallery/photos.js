82286f0e3b9a419ca1664f3ed7fd16efeedc7faf
TyroneHe-0926 <tyrone99926@gmail.com>
2026-08-03 19:51:26 -0400

    chore: updating gallery

--- src/components/gallery/photos.js
+++ src/components/gallery/photos.js
@@ -7,15 +7,7 @@ export const photos = [
     date: "July 12, 2020",
     iso: "2020-07-12",
     story:
-      "I went on a road trip from Vancouver to Calgary during Covid in 2020. This was a picture I took in front of the famous Lake Louise.",
-  },
-  {
-    src: "/images/gallery/toronto-1.jpg",
-    title: "Toronto, Ontario",
-    date: "February 25, 2024",
-    iso: "2024-02-25",
-    story:
-      "Caught a crazy sunset in Toronto. I was interning for RBC back then and lived in downtown Toronto for around 4 months. Can't say I like Toronto that much, but at the time of this picture, I just escaped from Waterloo after being in school for 1 year, so Toronto felt like a breath of fresh air.",
+      "Taken during the road trip from Vancouver to Calgary in 2020. This was during the Covid period, I couldn't really find an internship and all the interviews that was originally lined up got cancelled, so I just skipped my co-op term and went on a road trip to Calgary with the family.",
   },
   {
     src: "/images/gallery/stown-1.jpg",
@@ -23,15 +15,7 @@ export const photos = [
     date: "December 19, 2021",
     iso: "2021-12-19",
     story:
-      "This is the view from the Shanghai Citrix office at Lu Jia Zui. I was visiting a couple of other Shanghai tech company offices including Microsoft and eBay. All of the offices looks really nice, and lots of good food choices nearby.",
-  },
-  {
-    src: "/images/gallery/whiterock-1.jpg",
-    title: "White Rock, B.C.",
-    date: "December 22, 2023",
-    iso: "2023-12-22",
-    story:
-      "The Whiterock Pier at night. The pier is only a 10-min drive from my house, we sometimes just go and take a little walk near the shore. This was just a bit before Christmas as well so I think there was some sort of event going on. Lots of decorations that night.",
+      "Taken during my internship in Shanghai in 2021. This is the view from the Shanghai Citrix office at Lu Jia Zui. I was visiting a couple of other tech company offices in Shanghai at the time of the picture.",
   },
   {
     src: "/images/gallery/waterloo-1.jpg",
@@ -39,6 +23,54 @@ export const photos = [
     date: "September 16, 2023",
     iso: "2023-09-16",
     story:
-      "There is a park near the Waterloo campus, and there's a small zoo within the park. There are usually some alpacas in there, I was rocking the same hair style as my dude here in the picture back then.",
+      "Tken during my 5th study term at Waterloo. There is a park near the Waterloo campus (Waterloo Park), and there's a small zoo (or rather, an animal display section) within the park. There are usually some alpacas in there, I was rocking the same hair style as my dude here in the picture back then.",
+  },
+  {
+    src: "/images/gallery/whiterock-1.jpg",
+    title: "White Rock, B.C.",
+    date: "December 22, 2023",
+    iso: "2023-12-22",
+    story:
+      "Taken during my remote internship at home, at the Whiterock Pier. The pier is only a 10-min drive from my house, kinda the go-to spot to take a little walk and chill. This was just before Christmas so I think there was some sort of event going on.",
+  },
+  {
+    src: "/images/gallery/toronto-1.jpg",
+    title: "Toronto, Ontario",
+    date: "February 25, 2024",
+    iso: "2024-02-25",
+    story:
+      "Taken during my internship in downtown Toronto in 2024. Caught a crazy sunset. Can't say Toronto is my favorite city, but at the time of this picture, I just escaped from Waterloo after being in school for 1 year, so Toronto felt like a breath of fresh air.",
+  },
+  {
+    src: "/images/gallery/peanutbutter.jpg",
+    title: "Kitchener, Ontario",
+    date: "May 17, 2025",
+    iso: "2025-05-17",
+    story:
+      "Taken during my part time study term in Kitchener at home. My pet hamster, peanut butter, just chilling in his mushroom house.",
+  },
+  {
+    src: "/images/gallery/niagara.jpg",
+    title: "Niagara Falls, Ontario",
+    date: "May 18, 2025",
+    iso: "2025-05-18",
+    story:
+      "Taken during a visit to Niagara Falls from a long weekend. It was my first time checking out the Falls, weather wasn't the best that day, but still, it was quite a spectacular sight",
+  },
+  {
+    src: "/images/gallery/pac.jpg",
+    title: "Waterloo, Ontario",
+    date: "April 21, 2026",
+    iso: "2026-04-21",
+    story:
+      "Taken after the last final exam of my entire undergrad in PAC (Physical Activity Complex). PAC is where we usually take all of our exams. Needless to say this places haunts me in my nightmares sometimes, the relief I felt that day after finishing my last ever exam in my undergrad was insane.", 
+  },
+  {
+    src: "/images/gallery/blufferpark.jpg",
+    title: "Toronto, Ontario",
+    date: "August 3, 2026",
+    iso: "2026-08-03",
+    story:
+      "Taken after moving to Toronto (Scarborough) with my partner. We have both finally graduated and my partner is starting her fulltime job in Toronto. The Bluffer's Park is a 10 min drive from our place in Scarborough, so we decided to finally check it out after moving in for a month.", 
   },
 ];
